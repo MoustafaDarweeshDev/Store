5873a9d4b67f2a75680ab890d710fcdf8037ab54
tatta <tattarwsh@gmail.com>
2022-08-06 13:46:46 +0200

    initial commit

---
+++ e-commerce/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_commerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _db;
+        public ProductsController(StoreContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _db.Products.ToListAsync();
+
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _db.Products.FindAsync(id);
+        }
+    }
+}
